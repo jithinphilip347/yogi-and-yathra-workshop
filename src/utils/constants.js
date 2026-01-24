f3041fb11f6384ff1715f8edf9c0bb4d73a0d7a5
Achu Sivadasan <achusivadasan416@gmail.com>
2026-01-24 12:40:49 +0530

    constants change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-export const API_BASE_URL = "http://localhost:8000/api/";
-export const MEDIA_BASE_URL = "http://localhost:8000/storage/";
+// export const API_BASE_URL = "http://localhost:8000/api/";
+// export const MEDIA_BASE_URL = "http://localhost:8000/storage/";
 
 // export const PRODUCT_API_BASE_URL = "http://localhost:8001/api/";
 // export const PRODUCT_MEDIA_BASE_URL = "http://localhost:8001/storage/";
 
-// export const API_BASE_URL = "https://admin.varixialabs.com/workshopapi/public/api/";
-// export const MEDIA_BASE_URL = "https://admin.varixialabs.com/workshopapi/public/storage/";
+export const API_BASE_URL = "https://admin.varixialabs.com/workshopapi/public/api/";
+export const MEDIA_BASE_URL = "https://admin.varixialabs.com/workshopapi/public/storage/";
 export const PRODUCT_API_BASE_URL = "https://api.yogiandyathra.com/public/api/";
 export const PRODUCT_MEDIA_BASE_URL = "https://api.yogiandyathra.com/public";
